505323967d32fca1959141ae71bda5e33416d0a9
Nikesh Adhikari <adhikariraj016@gmail.com>
2026-02-12 16:34:07 +0545

    Changes in grammr logo

--- fiu-frontend/src/components/grammr.js
+++ fiu-frontend/src/components/grammr.js
@@ -1,19 +1,17 @@
 import Image from "next/image";
 
-export default function Grammr() {
+export default function Logo() {
   return (
-    <div className="w-24 xs:w-32 sm:w-40 md:w-52 lg:w-60 xl:w-[300px] max-w-full h-auto flex items-center">
-      
+    <div className="w-40 xs:w-52 sm:w-60 md:w-72 lg:w-80 xl:w-[350px] max-w-full h-auto flex items-center">
       <Image
         src="/images/grammr.png"
-        alt="GRAMMR Logo"
+        alt="FIU Logo"
         width={350}
         height={250}
         className="w-full h-auto "
-        sizes="(max-width: 480px) 96px, (max-width: 640px) 128px, (max-width: 768px) 160px, (max-width: 1024px) 208px, (max-width: 1280px) 240px, 350px"
+        sizes="(max-width: 480px) 160px, (max-width: 640px) 208px, (max-width: 768px) 240px, (max-width: 1024px) 288px, (max-width: 1280px) 320px, 350px"
         priority
-      /> 
-      
-      </div>
+      />
+    </div>
   );
 }
